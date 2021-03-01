c956fb9bf6e90bd34cc0a86f7a893d21d4574162
Patrick <prm5983@rit.edu>
2021-03-01 17:45:06 -0500

    Got movement working, started working on multiple jumps

---
+++ FleeTheReaper/Assets/Scripts/CameraSwivel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwivel : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 1;
+    private float currentSwivelAxis;
+
+    private void GetSwivelInput()
+    {
+        currentSwivelAxis = 0;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentSwivelAxis--;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentSwivelAxis++;
+        }
+    }
+
+    void Update()
+    {
+        if (!Mathf.Approximately(currentSwivelAxis, 0))
+        {
+            transform.Rotate(0, rotateSpeed * currentSwivelAxis * Time.deltaTime, 0);
+        }
+    }
+}
