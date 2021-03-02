379e8a69abcad599b5e26256c23a35535bb6df01
Patrick <prm5983@rit.edu>
2021-03-02 00:54:02 -0500

    Added enemy that chases and collectibles / score text

---
+++ FleeTheReaper/Assets/Scripts/ChaseTarget.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChaseTarget : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    [Tooltip("The distance this object moves per second towards target.")]
+    [SerializeField] private float speed;
+
+    private Camera cachedCam;
+
+    private void Update()
+    {
+        if (target)
+        {
+            transform.LookAt(target.transform, Vector3.up);
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                target.transform.position,
+                speed * Time.deltaTime
+            );
+        }
+        else
+        {
+            if (!cachedCam) { cachedCam = Camera.main; }
+            Vector3 targetPos = cachedCam.transform.position + cachedCam.transform.forward * 1.5f;
+
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                targetPos,
+                speed * Time.deltaTime
+            );
+
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation,
+                Quaternion.LookRotation(
+                    transform.position != targetPos
+                        ? targetPos - transform.position
+                        : cachedCam.transform.position - transform.position
+                    ),
+                Mathf.Rad2Deg * (speed * Time.deltaTime)
+            );
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(target))
+        {
+            Destroy(target);
+            StartCoroutine(DelayedRestart(2.5f));
+        }
+    }
+
+    private IEnumerator DelayedRestart(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ FleeTheReaper/Assets/Scripts/PlaceAndScore.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlaceAndScore : MonoBehaviour
+{
+    [SerializeField] private Collider spawnArea = null;
+    [SerializeField] private Collider thisCollider = null;
+    [SerializeField] private TextMeshProUGUI scoreText = null;
+    private int score = 0;
+
+    private void Start() { Relocate(); }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Relocate();
+            score++;
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+    private void Relocate()
+    {
+        Vector3 newPos = new Vector3(
+            Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x),
+            Random.Range(spawnArea.bounds.min.y, spawnArea.bounds.max.y),
+            Random.Range(spawnArea.bounds.min.z, spawnArea.bounds.max.z)
+        );
+
+        var overlaps = Physics.OverlapSphere(
+            newPos,
+            MaxComponent(thisCollider.bounds.extents) + 0.25f,
+            ~0,
+            QueryTriggerInteraction.Ignore
+        );
+
+        while (overlaps.Length > 0)
+        {
+            newPos.y = overlaps[0].bounds.max.y + thisCollider.bounds.extents.y + 0.3f;
+            overlaps = Physics.OverlapSphere(
+                newPos,
+                MaxComponent(thisCollider.bounds.extents) + 0.25f,
+                ~0,
+                QueryTriggerInteraction.Ignore
+            );
+        }
+
+        transform.position = newPos;
+        Debug.Log(newPos);
+    }
+
+    private float MaxComponent(Vector3 vector) { return Mathf.Max(vector.x, vector.y, vector.z); }
+}
